37cb6343129c392134861cf2fb1edd515bef7c95
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-27 11:55:05 -0400

    Update gong magnetogram source Id

--- install/database/2023_06_27_gong_magnetogram.sql
+++ install/database/2023_06_27_gong_magnetogram.sql
@@ -1,9 +1,9 @@
 INSERT INTO datasources (id, name, description, units, layeringOrder, enabled, sourceIdGroup, displayOrder)
 VALUES
-(95, 'GONG Magnetogram', 'GONG Magnetogram',  NULL, 1, 0, '', 0);
+(37, 'GONG Magnetogram', 'GONG Magnetogram',  NULL, 1, 0, '', 0);
 
 INSERT INTO datasource_property (sourceId, label, name, fitsName, description, uiOrder)
 VALUES
-(95, 'Observatory', 'GONG', 'NSO-GONG', 'GONG', 1),
-(95, 'Instrument', 'GONG', 'GONG', 'GONG', 2),
-(95, 'Measurement', 'magnetogram', 'magnetogram', 'GONG Magnetogram', 3);
+(37, 'Observatory', 'GONG', 'NSO-GONG', 'GONG', 1),
+(37, 'Instrument', 'GONG', 'GONG', 'GONG', 2),
+(37, 'Measurement', 'magnetogram', 'magnetogram', 'GONG Magnetogram', 3);
